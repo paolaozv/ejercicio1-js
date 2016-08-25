8e8591f71d843f2f3e5bc7c3c88934635004e10b
Paola Ortiz de Zevallos <paolaozv@gmail.com>
2016-08-25 17:02:14 -0500

    ejercicio 1

---
+++ app/app.js
@@ -0,0 +1,5 @@
+var num1 = prompt ("Dime un número");
+var num2 = prompt ("Dime otro número");
+
+document.write("La suma de "+ num1 + " y "+ num2 + " es: ");
+document.write(parseInt(num1)+parseInt(num2));
